e3c4ee7148a98525723c1782ab47be673dda2fa5
Museus <museus@protonmail.com>
2023-09-17 18:14:52 -0400

    feat: track last ranked, use cached data if no new runs have been verified

--- database.py
+++ database.py
@@ -1,18 +1,30 @@
+import datetime
 import sqlite3
 
 class Database:
 
     def __init__(self):
-        self.db = sqlite3.connect("users.db")
-        self.cur = self.db.cursor()
+        self.conn = sqlite3.connect("lattrank.db")
+        self.cur = self.conn.cursor()
 
     def initialize(self):
         existing_table = self.cur.execute("SELECT name FROM sqlite_master WHERE name='src_users'")
         if existing_table.fetchone() is None:
             print("Initializing user database...")
-            self.cur.execute("CREATE TABLE src_users(src_id, name)")
+            self.cur.execute("CREATE TABLE src_users(src_id, name, score)")
         else:
             print("Using existing user database...")
+
+        existing_table = self.cur.execute("SELECT name FROM sqlite_master WHERE name='last_ranked'")
+        if existing_table.fetchone() is None:
+            print("Initializing last ranking database...")
+            self.cur.execute("CREATE TABLE last_ranked(timestamp)")
+        else:
+            print("Using existing last ranking database...")
+
+    def get_all_user_data(self):
+        result = self.cur.execute(f"SELECT name, score FROM src_users").fetchall()
+        return result
 
     def get_user_name_by_id(self, src_id):
         result = self.cur.execute(f"SELECT name FROM src_users WHERE src_id='{src_id}'").fetchone()
@@ -21,8 +33,30 @@ class Database:
         else:
             return None
 
-    def add_user(self, src_id, user_name):
-        self.cur.execute(f"INSERT INTO src_users VALUES('{src_id}', '{user_name}')")
-        self.db.commit()
+    def get_user_score_by_id(self, src_id):
+        result = self.cur.execute(f"SELECT score FROM src_users WHERE src_id='{src_id}'").fetchone()
+        if result:
+            return result[0]
+        else:
+            return None
+
+    def add_user(self, src_id, user_name, score):
+        self.cur.execute(f"INSERT INTO src_users VALUES('{src_id}', '{user_name}', '{score}')")
+        self.conn.commit()
+
+    def update_score(self, src_id, new_score):
+        self.cur.execute(f"UPDATE src_users SET score = '{new_score}' WHERE src_id='{src_id}'")
+        self.conn.commit()
+
+    def get_last_ranked(self):
+        result = self.cur.execute(f"SELECT timestamp FROM last_ranked").fetchone()
+        if result:
+            return result[0]
+        else:
+            return None
+
+    def mark_last_ranked(self, latest_timestamp):
+        self.cur.execute(f"INSERT INTO last_ranked VALUES('{latest_timestamp}')")
+        self.conn.commit()
 
 db = Database()
--- speedrun.py
+++ speedrun.py
@@ -18,59 +18,80 @@ user_points = defaultdict(int)
 
 db.initialize()
 
-print("Getting all run data...")
+latest_verified_url = f"https://www.speedrun.com/api/v1/runs?game={GAME_ID}&status=verified&orderby=verify-date&direction=desc"
+latest_verified_runs = requests.get(latest_verified_url).json()
 
-for category_name, category_id in CATEGORIES.items():
-    log(category_name)
-    keys, values = zip(*SUBCATEGORIES[category_name].items())
-    permutations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
-    for combination in permutations_dicts:
-        for timing in TIMINGS[category_name]:
+latest_run = latest_verified_runs["data"][0]
+latest_ranked = db.get_last_ranked()
 
-            skip_this_one = False
+new_runs = True
 
-            mapped_subcategories = [SUBCATEGORIES_MAP[category_name][(var_id, var_value)] for var_id, var_value in combination.items()]
-            all_filters = mapped_subcategories + [TIMINGS_MAP[timing]]
+if latest_run["status"]["verify-date"] == latest_ranked:
+    print("No runs have been verified since last run. Returning existing stats.")
+    new_runs = False
 
-            subcategory_pub_string = ", ".join(mapped_subcategories)
-            for illegal_category in IGNORE.get(category_name, []):
-                if all(illegal_value in all_filters for illegal_value in illegal_category):
-                    log(f"  -- Skipping {TIMINGS_MAP[timing]}, {subcategory_pub_string}")
-                    skip_this_one = True
+if new_runs:
+    print("Getting all run data...")
 
-            if skip_this_one:
-                continue
+    for category_name, category_id in CATEGORIES.items():
+        log(category_name)
+        keys, values = zip(*SUBCATEGORIES[category_name].items())
+        permutations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
+        for combination in permutations_dicts:
+            for timing in TIMINGS[category_name]:
 
-            subcategory_var_string = "&".join([f"var-{var_id}={var_value}" for var_id, var_value in combination.items()])
-            subcategory_pub_string = ", ".join([SUBCATEGORIES_MAP[category_name][(var_id, var_value)] for var_id, var_value in combination.items()])
+                skip_this_one = False
 
-            log(f"  + Getting {TIMINGS_MAP[timing]}, {subcategory_pub_string}  [ /api/v1/leaderboards/{GAME_ID}/category/{category_id}?top=100&{subcategory_var_string}&timing={timing} ]")
-            connection_string = f"https://speedrun.com/api/v1/leaderboards/{GAME_ID}/category/{category_id}?top=100&{subcategory_var_string}&timing={timing}"
+                mapped_subcategories = [SUBCATEGORIES_MAP[category_name][(var_id, var_value)] for var_id, var_value in combination.items()]
+                all_filters = mapped_subcategories + [TIMINGS_MAP[timing]]
 
-            top_x_runs = requests.get(connection_string).json()
-            unique_players = set()
+                subcategory_pub_string = ", ".join(mapped_subcategories)
+                for illegal_category in IGNORE.get(category_name, []):
+                    if all(illegal_value in all_filters for illegal_value in illegal_category):
+                        log(f"  -- Skipping {TIMINGS_MAP[timing]}, {subcategory_pub_string}")
+                        skip_this_one = True
 
-            for run in top_x_runs["data"]["runs"]:
-                user_id = run["run"]["players"][0]["id"]
-                if user_id in unique_players:
+                if skip_this_one:
                     continue
 
-                user_leaderboard[user_id]["score"] += 10 - len(unique_players)
-                user_leaderboard[user_id]["runs"][run["run"]["weblink"]] = (f"{category_name} ({TIMINGS_MAP[timing]}, {subcategory_pub_string})", 10 - len(unique_players))
-                unique_players.add(user_id)
-                if len(unique_players) == 10:
-                    break
+                subcategory_var_string = "&".join([f"var-{var_id}={var_value}" for var_id, var_value in combination.items()])
+                subcategory_pub_string = ", ".join([SUBCATEGORIES_MAP[category_name][(var_id, var_value)] for var_id, var_value in combination.items()])
 
-place_width = len(str(len([user for user in user_leaderboard.keys() if user_leaderboard[user]["score"] >= 5]))) + 1
+                log(f"  + Getting {TIMINGS_MAP[timing]}, {subcategory_pub_string}  [ /api/v1/leaderboards/{GAME_ID}/category/{category_id}?top=100&{subcategory_var_string}&timing={timing} ]")
+                connection_string = f"https://speedrun.com/api/v1/leaderboards/{GAME_ID}/category/{category_id}?top=100&{subcategory_var_string}&timing={timing}"
 
-for place, user_id in enumerate(sorted(user_leaderboard, key=lambda x: user_leaderboard[x]["score"], reverse=True)):
-    user_name = db.get_user_name_by_id(user_id)
-    if not user_name:
-        user_name = requests.get(f"https://speedrun.com/api/v1/users/{user_id}").json()["data"]["names"]["international"]
-        db.add_user(user_id, user_name)
-        time.sleep(1)
+                top_x_runs = requests.get(connection_string).json()
+                unique_players = set()
 
-    if user_leaderboard[user_id]['score'] < 5:
-        break
+                for run in top_x_runs["data"]["runs"]:
+                    user_id = run["run"]["players"][0]["id"]
+                    if user_id in unique_players:
+                        continue
 
-    print(f"{str(place+1).rjust(place_width)} - {user_name} - {user_leaderboard[user_id]['score']}")
+                    user_leaderboard[user_id]["score"] += 10 - len(unique_players)
+                    user_leaderboard[user_id]["runs"][run["run"]["weblink"]] = (f"{category_name} ({TIMINGS_MAP[timing]}, {subcategory_pub_string})", 10 - len(unique_players))
+                    unique_players.add(user_id)
+                    if len(unique_players) == 10:
+                        break
+
+    db.mark_last_ranked(latest_run["status"]["verify-date"])
+
+    place_width = len(str(len([user for user in user_leaderboard.keys() if user_leaderboard[user]["score"] >= 5]))) + 1
+
+    for place, user_id in enumerate(sorted(user_leaderboard, key=lambda x: user_leaderboard[x]["score"], reverse=True)):
+        user_name = db.get_user_name_by_id(user_id)
+        if not user_name:
+            user_name = requests.get(f"https://speedrun.com/api/v1/users/{user_id}").json()["data"]["names"]["international"]
+            db.add_user(user_id, user_name, user_leaderboard[user_id]['score'])
+            time.sleep(1)
+        else:
+            db.update_score(user_id, user_leaderboard[user_id]['score'])
+
+        print(f"{str(place+1).rjust(place_width)} - {user_name} - {user_leaderboard[user_id]['score']}")
+
+else:
+    all_user_data = db.get_all_user_data()
+    place_width = len(str(len(all_user_data)))
+
+    for place, user in enumerate(sorted(all_user_data, key=lambda x: int(x[1] or "0"), reverse=True)):
+        print(f"{str(place+1).rjust(place_width)} - {user[0]} - {user[1]}")
